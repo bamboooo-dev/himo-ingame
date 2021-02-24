8ce6c2d6ad0a33d31092a39bdfecc66bcdd55189
yamagai <kohei.yamamoto919@gmail.com>
2021-02-24 20:33:03 +0900

    template から環境変数を読み込むための処理追加

--- cmd/ingame/main.go
+++ cmd/ingame/main.go
@@ -1,13 +1,36 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/bamboooo-dev/himo-ingame/internal/interface/router"
+	"github.com/bamboooo-dev/himo-ingame/pkg/env"
 )
 
+// from LDFLAGS
+var revision = "undefined"
+
+// from flag
+var templateFilePath string
+
 func main() {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Printf("Panic '%v' captured\n", err)
+		}
+	}()
+
+	fmt.Printf("Version is %s\n", revision)
+	flag.StringVar(&templateFilePath, "f", "config/application.yml.tpl", "path of config template")
+	flag.Parse()
+
+	cfg, err := env.LoadConfigFromTemplate(templateFilePath)
+	if err != nil {
+		panic(err)
+	}
 
 	s := router.NewServer()
 
--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module github.com/bamboooo-dev/himo-ingame
 
 go 1.15
 
-require github.com/gorilla/websocket v1.4.2
+require (
+	github.com/bamboooo-dev/himo-outgame v0.0.0-20210220180821-b1e521eddc1f
+	github.com/go-gorp/gorp v2.2.0+incompatible
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/gorilla/websocket v1.4.2
+	github.com/jinzhu/configor v1.2.1
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1
+)
